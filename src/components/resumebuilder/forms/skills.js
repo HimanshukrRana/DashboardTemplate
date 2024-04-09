757042b9b1721cb2efd1701d245a3165248ce84c
HimanshukrRana <hr.himanshu143@gmail.com>
2024-04-09 11:37:04 +0530

    skills   update

--- src/components/resumebuilder/forms/skills.js
+++ src/components/resumebuilder/forms/skills.js
@@ -1,6 +1,48 @@
+import { updateDataByKey } from "@/store/actions";
 import React from "react";
+import CreatableSelect from "react-select/creatable";
+
+import { useDispatch, useSelector } from "react-redux";
+
+const components = {
+  DropdownIndicator: null,
+};
+
+const createOption = (label) => ({
+  label,
+  value: label,
+});
 
 export default function Skills() {
+  const [inputValue, setInputValue] = React.useState("");
+  const [value, setValue] = React.useState([]);
+
+  const dispatch = useDispatch();
+  const storeData = useSelector((state) => state.common);
+
+  console.log(storeData, "storedata");
+
+  const handleKeyDown = (event) => {
+    if (!inputValue) return;
+    switch (event.key) {
+      case "Enter":
+      case "Tab":
+        setValue((prev) => [...prev, createOption(inputValue)]);
+        setInputValue("");
+        event.preventDefault();
+    }
+  };
+
+  function handleClick() {
+    dispatch(
+      updateDataByKey("resumeData", {
+        ...storeData.resumeData,
+        skills: value,
+      })
+    );
+    console.log(value, "submitted values");
+  }
+
   return (
     <div>
       <div>
@@ -13,17 +55,33 @@ export default function Skills() {
               <label htmlFor="skills">Skills *</label>
             </div>
             <div>
-              <input
+              {/* <input
                 id="skills"
                 type="text"
                 className="px-2 border border-gray-200 rounded-lg w-full h-[40px]"
                 placeholder=""
+              /> */}
+              <CreatableSelect
+                components={components}
+                inputValue={inputValue}
+                isClearable
+                isMulti
+                menuIsOpen={false}
+                onChange={(newValue) => setValue(newValue)}
+                onInputChange={(newValue) => setInputValue(newValue)}
+                onKeyDown={handleKeyDown}
+                placeholder="Type your Skills"
+                value={value}
+                className="px-2  rounded-lg w-full h-[40px]"
               />
             </div>
           </div>
         </div>
         <div className="p-4 flex justify-end border-t border-gray-200 absolute bottom-0 right-0 left-0">
-          <button className="px-6 py-2 text-lg rounded-lg  bg-black border text-white">
+          <button
+            onClick={handleClick}
+            className="px-6 py-2 text-lg rounded-lg  bg-black border text-white"
+          >
             SAVE
           </button>
         </div>
--- src/components/resumebuilder/forms/summary.js
+++ src/components/resumebuilder/forms/summary.js
@@ -1,6 +1,14 @@
+import InputComponent from "@/components/input";
+import { updateDataByKey } from "@/store/actions";
+import { Formik } from "formik";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Summary() {
+  const dispatch = useDispatch();
+  const storeData = useSelector((state) => state.common);
+
+  console.log(storeData, "storedata");
   return (
     <div className="">
       <div>
@@ -9,23 +17,67 @@ export default function Summary() {
         </div>
         <div>
           <div className="py-2 px-8">
-            <div className="mb-1">
-              <label htmlFor="summary">Summary</label>
-            </div>
-            <div>
-              <textarea
-                id="summary"
-                type="text"
-                className="px-2 border border-gray-200 rounded-lg w-full h-[140px]"
-                placeholder=""
-              />
-            </div>
+            <Formik
+              enableReinitialize
+              initialValues={{
+                summary: "",
+              }}
+              onSubmit={(values) => {
+                dispatch(
+                  updateDataByKey("resumeData", {
+                    ...storeData.resumeData,
+                    summary: values,
+                  })
+                );
+              }}
+            >
+              {({
+                handleSubmit,
+                handleChange,
+                touched,
+                errors,
+                values,
+                setFieldValue,
+              }) => {
+                return (
+                  <div>
+                    <div className="mb-1">
+                      <label htmlFor="">Summary</label>
+                    </div>
+                    <div>
+                      {/* <textarea
+                        id="details"
+                        type="text"
+                        placeholder="Enter Your Summar Details"
+                        value={values.summary}
+                        name="summary"
+                        onChange={handleChange}
+                      /> */}
+                      <InputComponent
+                        type="textArea"
+                        value={values.summary}
+                        name="summary"
+                        onChange={handleChange}
+                        placeholder=""
+                        is_error={errors.summary}
+                        errorMessage={errors.summary}
+                        className="px-2 border border-gray-200 rounded-lg w-full h-[140px]"
+                      />
+                    </div>
+
+                    <div className="p-4 flex justify-end border-t border-gray-200 absolute bottom-0 right-0 left-0">
+                      <button
+                        onClick={handleSubmit}
+                        className="px-6 py-2 text-lg rounded-lg  bg-black border text-white"
+                      >
+                        SAVE
+                      </button>
+                    </div>
+                  </div>
+                );
+              }}
+            </Formik>
           </div>
-        </div>
-        <div className="p-4 flex justify-end border-t border-gray-200 absolute bottom-0 right-0 left-0">
-          <button className="px-6 py-2 text-lg rounded-lg  bg-black border text-white">
-            SAVE
-          </button>
         </div>
       </div>
     </div>
